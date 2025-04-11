a230f5b19446d4ee09cd05bf7e02d13e178323b5
saymon1903 <madphonk99@gmail.com>
2025-04-11 17:33:24 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ console.log("Student Marketplace loaded successfully!");
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Website loaded successfully!");
 });
+document.getElementById('searchBar').addEventListener('input', function() {
+  const value = this.value.toLowerCase();
+  const cards = document.querySelectorAll('.product-card');
+  cards.forEach(card => {
+    const title = card.querySelector('h3').innerText.toLowerCase();
+    card.style.display = title.includes(value) ? 'block' : 'none';
+  });
+});
 
