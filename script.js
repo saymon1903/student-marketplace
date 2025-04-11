04500c14c70c419660550bfd54b1ed9b03f36502
saymon1903 <madphonk99@gmail.com>
2025-04-11 16:15:31 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 // Add interactivity here if needed
 console.log("Student Marketplace loaded successfully!");
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Website loaded successfully!");
+});
 
