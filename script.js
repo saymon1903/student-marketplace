bd13e815a83efc1b06a672486a336fb6af354629
student marketplace <madphonk99@gmail.com>
2025-04-14 11:42:33 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
-// সাইন আপ ফর্মের ডেটা সংগ্রহ এবং প্রক্রিয়া করার জন্য ফাংশন
-function processSignupForm(event) {
-  event.preventDefault();
-
-  const formData = new FormData(event.target);
-  const userType = formData.get('userType');
-  const userData = {};
-
-  formData.forEach((value, key) => {
-    userData[key] = value;
-  });
-
-  // ইউজার ডেটা প্রক্রিয়া এবং সেভ করার কোড এখানে যুক্ত করুন
-
-  alert(`${userType} হিসেবে সাইন আপ সফল হয়েছে!`);
-}
-
-// সাইন আপ ফর্মের সাবমিট ইভেন্ট লিসেনার
-document.querySelector('form').addEventListener('submit', processSignupForm);
 // script.js
 
 document.querySelectorAll(".btn").forEach(button => {
   button.addEventListener("click", () => {
-    alert("This will take you to the signup page. Coming soon!");
+    alert("Buy Now Clicked!");
   });
 });
